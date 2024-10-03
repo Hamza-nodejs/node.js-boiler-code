55fcf7145fa53ba8d4d8146aafef3ac415ea7424
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-03 11:12:48 +0500

    controllers added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,11 @@ const { HTTPResponse, HTTPError } = require("../utils/response")
 const statusCodes = require("http-status-codes")
 
 
-const protectedUser = async (req, res) => {
-    let response = new HTTPResponse("protected user response", statusCodes.OK)
+const userApi = async (req, res) => {
+    let response = new HTTPResponse("public user response", statusCodes.OK)
     return res.status(statusCodes.OK).json(response);
 }
+
 module.exports = {
-    protectedUser
+    userApi
 }
